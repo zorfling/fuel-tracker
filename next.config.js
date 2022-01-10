089a9cb4b757f4fc170539deee8addae873b44a7
Chris Colborne <zorfling@gmail.com>
2022-01-10 14:07:41 +1000

    Fix some logos

--- next.config.js
+++ next.config.js
@@ -5,12 +5,14 @@ module.exports = {
     domains: [
       'upload.wikimedia.org',
       'logok.org',
-      'cpp-prod-seek-company-image-uploads.s3.ap-southeast-2.amazonaws.com',
+      'www.parkridgetowncentre.com.au',
       'fuelprice.io',
       'cdn.australia247.info',
       'www.pacificpetroleum.com.au',
       'www.kdpr.com.au',
-      'i.pinimg.com'
+      'i.pinimg.com',
+      'www.libertyoil.com.au',
+      'metropetroleum.b-cdn.net'
     ]
   }
 };
